23254fca7d6a88cfa1ad0505c9d61727fff8022f
JoseOrdaz <joseordazsuay@gmail.com>
2022-01-23 20:34:32 +0100

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <main>
+      tiktok clone
+
+      </main>
+   
+    </div>
+  )
+}
+
+export default App
