fd590a6cc5febaac5cc5788b73f2b2e90ea6462b
JoseOrdaz <joseordazsuay@gmail.com>
2022-01-31 19:37:20 +0100

    añdiendo video y boton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import VideoPlayer from './components/VideoPlayer'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <main>
-      tiktok clone
-      
+      <main>    
+      <VideoPlayer />
       </main>
    
     </div>
